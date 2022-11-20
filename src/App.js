f8d7da565b4eb9e6f8d27f5691610dbda79d260b
HyunseokLEE <unducklee@gmail.com>
2022-11-20 23:16:28 +0900

    feat/[layout-n]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Layout from './component_layout/layout';
 
 function App() {
   return (
---
+++ src/component_layout/footer.js
@@ -0,0 +1,13 @@
+import './footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="contents">
+        <h2 className="title">이것은 푸터</h2>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
