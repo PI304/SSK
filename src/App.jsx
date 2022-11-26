8e7443c4f246d3870664e1536bf86b77b8864a05
HyunseokLEE <unducklee@gmail.com>
2022-11-26 23:10:23 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
 import Layout from './component_layout/layout';
-import styles from './styles/reset.module.css';
 
 function App() {
 	return (
