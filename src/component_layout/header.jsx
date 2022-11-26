44992a340bb9723d2aacd5f83ada3b0a4fa46aad
HyunseokLEE <unducklee@gmail.com>
2022-11-26 13:57:50 +0900

    add/photo

--- src/component_layout/header.jsx
+++ src/component_layout/header.jsx
@@ -4,15 +4,15 @@ function Header() {
 	return (
 		<HeaderContainerCss>
 			<Nav>
+				<div />
 				<NavMenuCss>
 					<NavMenuUlCss>
-						<li>ADDS 소개</li>
-						<li>발간물</li>
-						<li>데이터</li>
-						<li>소통공간</li>
+						<NavMenuLiCss>ADDS 소개</NavMenuLiCss>
+						<NavMenuLiCss>발간물</NavMenuLiCss>
+						<NavMenuLiCss>데이터</NavMenuLiCss>
+						<NavMenuLiCss>소통공간</NavMenuLiCss>
 					</NavMenuUlCss>
 				</NavMenuCss>
-				<div />
 				<div />
 			</Nav>
 		</HeaderContainerCss>
@@ -21,27 +21,32 @@ function Header() {
 
 const HeaderContainerCss = styled.header`
 	box-shadow: 0px 2px 10px rgb(0 0 0 / 10%);
-	flex: 0 0 auto;
-	justify-content: center;
 `;
 
 const Nav = styled.nav`
 	margin: auto;
 	width: 73.438rem;
+	height: 5.75rem;
 	display: grid;
 	grid-template-columns: 1fr 7fr 1fr;
 	align-items: center;
 `;
 
 const NavMenuCss = styled.div`
-	height: 5.75rem;
 	width: fit-content;
 	justify-self: end;
+	margin-right: 4rem;
 `;
 
 const NavMenuUlCss = styled.ul`
 	display: flex;
 	justify-content: flex-end;
+	gap: 1rem 5rem;
+	list-style: none;
+	margin: 0;
+	padding: 0;
 `;
+
+const NavMenuLiCss = styled.li``;
 
 export default Header;
