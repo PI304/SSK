d6ff70f303ab0220c78b0bb518126af2178ecf95
HyunseokLEE <unducklee@gmail.com>
2022-12-09 19:12:05 +0900

    feat: 공동 컴포넌트 생성

---
+++ src/components/component_layout/MainContainer.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const MainContainerCss = styled.div`
+	display: grid;
+	margin: 3rem auto;
+	margin-top: 6rem;
+	margin-bottom: 6rem;
+	width: 120rem;
+	height: 164.63rem;
+	grid-template-columns: 18rem 99rem;
+	grid-gap: 3rem;
+`;
+
+export default MainContainerCss;
