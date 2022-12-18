bc3b384dc8d2aef57e78dc740faa715dd0f127c2
dmsthf2000 <dsmthf200099@gmail.com>
2022-12-18 23:42:50 +0900

    fix: SSelectOptionS 컴포넌트 css 수정

--- src/components/Notice/SelectOption.js
+++ src/components/Notice/SelectOption.js
@@ -4,7 +4,7 @@ import Colors from '../../constants/colors';
 const SSelectOptionS = styled.select`
     float: right;
     width: 11.6rem;
-    padding: 0.4rem;
+    padding: 0 0.4rem;
 	color: ${Colors.select1};
 	border-color: ${Colors.select2};
 `;
