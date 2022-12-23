0c4bcebedcc5155f556a8864bf2f3736860992b7
HyunseokLEE <unducklee@gmail.com>
2022-12-24 01:32:45 +0900

    feat: 캐러쉘 기능 추가

--- src/components/Main/MainCarousel_2.jsx
+++ src/components/Main/MainCarousel_2.jsx
@@ -10,44 +10,94 @@ import ArrowLeft from '../../assets/arrow_back_ios_FILL0_wght400_GRAD0_opsz48.pn
 import Colors from '../../constants/colors';
 
 function MainCarousel() {
+	const TOTAL_SLIDES = 4;
+	const [isSlide, setIsSlide] = useState(0);
+	const slideRef = useRef(null);
+
+	// Next 버튼 클릭 시
+	const NextSlide = () => {
+		if (isSlide >= TOTAL_SLIDES) {
+			setIsSlide(0);
+		} else {
+			setIsSlide(isSlide + 1);
+		}
+	};
+
+	// Prev 버튼 클릭시
+	const PrevSlide = () => {
+		if (isSlide === 0) {
+			setIsSlide(TOTAL_SLIDES);
+		} else {
+			setIsSlide(isSlide - 1);
+		}
+	};
+
+	// Slider의 정보
+	useEffect(() => {
+		slideRef.current.style.transition = 'all 0.5s ease-in-out';
+		slideRef.current.style.transform = `translateX(-${isSlide}00%)`;
+	}, [isSlide]);
+
 	return (
-		<CarouselContainer>
-			<div>
-				<ArrowB>
-					<img src={ArrowLeft} alt='arrowleft' />
-				</ArrowB>
-			</div>
+		<div>
+			<CarouselContainer>
+				<div>
+					<SArrowB onClick={PrevSlide}>
+						<img src={ArrowLeft} alt='arrowleft' width='60%' />
+					</SArrowB>
+				</div>
 
-			<ContentContainer>
-				<ContentBox>
-					<ContentIMG src={IMG1} alt='IMG1' />
-				</ContentBox>
-				<ContentBox>
-					<ContentIMG src={IMG2} alt='IMG2' />
-				</ContentBox>
-				<ContentBox>
-					<ContentIMG src={IMG3} alt='IMG3' />
-				</ContentBox>
-				<ContentBox>
-					<ContentIMG src={IMG4} alt='IMG4' />
-				</ContentBox>
-				<ContentBox>
-					<ContentIMG src={IMG5} alt='IMG5' />
-				</ContentBox>
-			</ContentContainer>
+				<ContentWindow ref={slideRef}>
+					<SContentWrapperD>
+						<SContentBoxD>
+							<a href='https://www.yonsei.ac.kr/sc/' target='_blank' rel='noreferrer'>
+								<SContentIMG src={IMG1} alt='IMG1' />
+							</a>
+						</SContentBoxD>
+						<SContentBoxD>
+							<a href='https://www.nrf.re.kr/index' target='_blank' rel='noreferrer'>
+								<SContentIMG src={IMG2} alt='IMG2' />
+							</a>
+						</SContentBoxD>
+						<SContentBoxD>
+							<a href='https://yonsei-impact.weebly.com/' target='_blank' rel='noreferrer'>
+								<SContentIMG src={IMG3} alt='IMG3' />
+							</a>
+						</SContentBoxD>
+						<SContentBoxD>
+							<a href='http://hlid.yonsei.ac.kr/' target='_blank' rel='noreferrer'>
+								<SContentIMG src={IMG4} alt='IMG4' />
+							</a>
+						</SContentBoxD>
+						<SContentBoxD>
+							<a href='http://sskn.kr/main/main.php' target='_blank' rel='noreferrer'>
+								<SContentIMG src={IMG5} alt='IMG5' />
+							</a>
+						</SContentBoxD>
+					</SContentWrapperD>
+				</ContentWindow>
 
-			<div>
-				<ArrowB>
-					<img src={ArrowRight} alt='arrowright' />
-				</ArrowB>
-			</div>
-		</CarouselContainer>
+				<div>
+					<SArrowB onClick={NextSlide}>
+						<img src={ArrowRight} alt='arrowright' width='60%' />
+					</SArrowB>
+				</div>
+			</CarouselContainer>
+
+			<SCircleContainer>
+				<SCircleD />
+				<SCircleD />
+				<SCircleD />
+				<SCircleD />
+				<SCircleD />
+			</SCircleContainer>
+		</div>
 	);
 }
 
 const CarouselContainer = styled.div`
 	width: 120rem;
-	height: 20.089rem;
+	height: 14.089rem;
 	padding: 0 5rem;
 	display: flex;
 	align-items: center;
@@ -55,7 +105,7 @@ const CarouselContainer = styled.div`
 `;
 
 /* Arrow Button */
-const ArrowB = styled.button`
+const SArrowB = styled.button`
 	border: 0;
 	color: black;
 	background-color: transparent;
@@ -63,11 +113,21 @@ const ArrowB = styled.button`
 `;
 
 /* Content */
-const ContentContainer = styled.div`
+const ContentWindow = styled.div`
+	width: 99.4rem;
+	height: 11.449rem;
+	overflow: hidden;
 	display: flex;
 `;
 
-const ContentBox = styled.div`
+const SContentWrapperD = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 6.5rem;
+`;
+
+const SContentBoxD = styled.div`
 	width: 25.6rem;
 	height: 9.248rem;
 	margin: 1.101rem;
@@ -75,11 +135,30 @@ const ContentBox = styled.div`
 	align-items: center;
 `;
 
-const ContentIMG = styled.img`
+const SContentIMG = styled.img`
 	max-width: 100%;
 	max-height: 100%;
 	object-fit: cover;
 	z-index: 1;
 `;
 
+/* 캐러쉘 circle */
+const SCircleContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	width: 9.6rem;
+	margin: auto;
+	margin-bottom: 7.2rem;
+`;
+
+const SCircleD = styled.div`
+	border: 1px solid ${Colors.black};
+	border-radius: 50%;
+	width: 0.64rem;
+	height: 0.64rem;
+`;
+
+const SBlackCircleD = styled.div`
+	background-color: ${Colors.black};
+`;
 export default MainCarousel;
