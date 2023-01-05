f41ecc2bc62c58d0afe961cebac1f109ce233cf5
tacowasabii <godseoril812@gmail.com>
2023-01-06 00:03:59 +0900

    Feature: 사이드바 아코디언 적용중

--- src/components/component_layout/Sidebar.jsx
+++ src/components/component_layout/Sidebar.jsx
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
+import MuiAccordion from '@mui/material/Accordion';
 import SidebarInner from './SidebarInner';
 import urls from '../../constants/Urls';
 
 function Sidebar() {
+	const [expanded, setExpanded] = useState(false);
+
+	const handleChange = (panel) => (event, isExpanded) => {
+		setExpanded(isExpanded ? panel : false);
+	};
 
 	const TitleUrl = {
 		intro: [urls.intro, urls.members, urls.research],
@@ -14,38 +22,110 @@ function Sidebar() {
 
 	const currentURL = useLocation().pathname;
 
+	// const Accordion = styled(MuiAccordion)(({ theme }) => ({
+	// 	'.MuiAccordion-root': {
+	// 		boxShadow: 'none',
+	// 	},
+	// }));
+
 	return (
 		<div>
-			<SSidebarTitleD currentURL={currentURL} location={TitleUrl.intro} to={urls.intro}>
-				ADDS소개
-			</SSidebarTitleD>
-			<SSidebarEntireListU>
-				<SidebarInner to={urls.intro} category='ADDS소개' />
-				<SidebarInner to={urls.members} category='연구진' />
-				<SidebarInner to={urls.research} category='조사설계' />
-			</SSidebarEntireListU>
-			<SSidebarTitleD currentURL={currentURL} location={TitleUrl.publication} to={urls.publication}>
-				발간물
-			</SSidebarTitleD>
-			<SSidebarEntireListU>
-				<SidebarInner to={urls.publication} category='발간물' />
-			</SSidebarEntireListU>
-			<SSidebarTitleD currentURL={currentURL} location={TitleUrl.resources} to={urls.resources}>
-				데이터
-			</SSidebarTitleD>
-			<SSidebarEntireListU>
-				<SidebarInner to={urls.resources} category='자료안내' />
-				<SidebarInner to={urls.papers} category='데이터활용논문' />
-			</SSidebarEntireListU>
-			<SSidebarTitleD currentURL={currentURL} location={TitleUrl.notice} to={urls.notice}>
-				소통공간
-			</SSidebarTitleD>
-			<SSidebarEntireListU>
-				<SidebarInner to={urls.notice} category='공지사항' />
-				<SidebarInner to={urls.reports} category='보도자료' />
-				<SidebarInner to={urls.FAQ} category='FAQ' />
-				<SidebarInner to={urls.contact} category='contact' />
-			</SSidebarEntireListU>
+			<Accordion
+				sx={{
+					boxShadow: 'none',
+				}}
+				disableGutters
+				expanded={expanded === 'panel1'}
+				onChange={handleChange('panel1')}>
+				<AccordionSummary
+					sx={{
+						padding: 0,
+					}}>
+					<SSidebarTitleD currentURL={currentURL} location={TitleUrl.intro} to={urls.intro}>
+						ADDS소개
+					</SSidebarTitleD>
+				</AccordionSummary>
+				<AccordionDetails>
+					<SSidebarEntireListU>
+						<SidebarInner to={urls.intro} category='ADDS소개' />
+						<SidebarInner to={urls.members} category='연구진' />
+						<SidebarInner to={urls.research} category='조사설계' />
+					</SSidebarEntireListU>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion
+				sx={{
+					boxShadow: 'none',
+				}}
+				disableGutters
+				expanded={expanded === 'panel2'}
+				onChange={handleChange('panel2')}>
+				<AccordionSummary
+					sx={{
+						padding: 0,
+					}}>
+					<SSidebarTitleD
+						currentURL={currentURL}
+						location={TitleUrl.publication}
+						to={urls.publication}>
+						발간물
+					</SSidebarTitleD>
+				</AccordionSummary>
+				<AccordionDetails>
+					<SSidebarEntireListU>
+						<SidebarInner to={urls.publication} category='발간물' />
+					</SSidebarEntireListU>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion
+				sx={{
+					boxShadow: 'none',
+				}}
+				disableGutters
+				expanded={expanded === 'panel3'}
+				onChange={handleChange('panel3')}>
+				<AccordionSummary
+					sx={{
+						padding: 0,
+					}}>
+					<SSidebarTitleD currentURL={currentURL} location={TitleUrl.resources} to={urls.resources}>
+						데이터
+					</SSidebarTitleD>
+				</AccordionSummary>
+				<AccordionDetails>
+					<SSidebarEntireListU>
+						<SidebarInner to={urls.resources} category='자료안내' />
+						<SidebarInner to={urls.papers} category='데이터활용논문' />
+					</SSidebarEntireListU>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion
+				sx={{
+					boxShadow: 'none',
+				}}
+				disableGutters
+				expanded={expanded === 'panel4'}
+				onChange={handleChange('panel4')}>
+				<AccordionSummary
+					sx={{
+						padding: 0,
+						'&.MuiAccordionSummary-content': {
+							margin: 0,
+						},
+					}}>
+					<SSidebarTitleD currentURL={currentURL} location={TitleUrl.notice} to={urls.notice}>
+						소통공간
+					</SSidebarTitleD>
+				</AccordionSummary>
+				<AccordionDetails>
+					<SSidebarEntireListU>
+						<SidebarInner to={urls.notice} category='공지사항' />
+						<SidebarInner to={urls.reports} category='보도자료' />
+						<SidebarInner to={urls.FAQ} category='FAQ' />
+						<SidebarInner to={urls.contact} category='contact' />
+					</SSidebarEntireListU>
+				</AccordionDetails>
+			</Accordion>
 		</div>
 	);
 }
