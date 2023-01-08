e524cbf0a70e67d98da46f79e7f0d1a402b687a1
tacowasabii <godseoril812@gmail.com>
2023-01-08 21:38:18 +0900

    Feature: main min-height 추가

--- src/components/component_layout/AddsLayout.jsx
+++ src/components/component_layout/AddsLayout.jsx
@@ -1,20 +1,27 @@
 import { Outlet, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
 import STitle from './Title';
 import Sidebar from './Sidebar';
 import pageTitle from '../../constants/pageTitle';
 import MainContainer from './MainContainer';
 
 function AddsLayout() {
-    const locationHook = useLocation();
-    const path = locationHook.pathname;
-    const title = path.split('/').reverse()[0];
-    return (
-        <MainContainer>
-            <STitle>{pageTitle[title]}</STitle>
-            <Sidebar/>
-            <Outlet/>
-        </MainContainer>
-    );
+	const locationHook = useLocation();
+	const path = locationHook.pathname;
+	const title = path.split('/').reverse()[0];
+	return (
+		<Main>
+			<MainContainer>
+				<STitle>{pageTitle[title]}</STitle>
+				<Sidebar />
+				<Outlet />
+			</MainContainer>
+		</Main>
+	);
 }
 
+const Main = styled.div`
+	min-height: 100vh;
+	flex: 1 1 auto;
+`;
 export default AddsLayout;
